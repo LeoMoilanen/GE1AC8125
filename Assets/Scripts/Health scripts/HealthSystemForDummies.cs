29bb79c34d0071dec763222de4f36285faa1cf33
Kimmo Riipinen <ad1900@student.jamk.fi>
2023-04-15 12:44:04 +0300

    Death scene added and death cutscene is working

--- Assets/Scripts/Health scripts/HealthSystemForDummies.cs
+++ Assets/Scripts/Health scripts/HealthSystemForDummies.cs
@@ -57,6 +57,6 @@ public class HealthSystemForDummies : MonoBehaviour
         OnIsAliveChanged.Invoke(IsAlive);
         OnCurrentHealthChanged.Invoke(new CurrentHealth(previousHealth, CurrentHealth, CurrentHealthPercentage));
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(2);
     }
 }
