60b8cbafb529b0f399b8ade16d9e720f797d6664
Kimmo Riipinen <ad1900@student.jamk.fi>
2023-04-16 22:59:52 +0300

    small update

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -9,12 +9,6 @@ public class GameTimer : MonoBehaviour
     [SerializeField] private float timer;
     [SerializeField] private GameObject waveSpawner;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
